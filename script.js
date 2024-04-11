d08248d2d728df3e91c5306b600cbf26732ae040
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-04-11 10:18:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
 //complete this code
 class Rectangle {
-	constructor(width,height){
-		this.width= width;
-		this.height=height;
-	}
-	get width(){
-		return this.width;
-	}
-	get height(){
-		return this.height;
-	}
-	getArea(){
-	return	`${this.width} * ${this.height}`;
-	}
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
 class Square extends Rectangle {
-	getPerimeter(){
-return `4 * ${this.width}`;
-	}
+  constructor(side) {
+    super(side, side);
+    this._side = side;
+  }
+
+  getPerimeter() {
+    return 4 * this._side;
+  }
 }
+// class Rectangle {
+// 	constructor(width,height){
+// 		this.width= width;
+// 		this.height=height;
+// 	}
+// 	get width(){
+// 		return this.width;
+// 	}
+// 	get height(){
+// 		return this.height;
+// 	}
+// 	getArea(){
+// 	return	`${this.width} * ${this.height}`;
+// 	}
+// }
+
+// class Square extends Rectangle {
+// 	getPerimeter(){
+// return `4 * ${this.width}`;
+// 	}
+// }
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
