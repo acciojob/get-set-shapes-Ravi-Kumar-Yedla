042b33d9d4c0741268017d093b51ae12640455dd
Ravi-Kumar-Yedla <136640114+Ravi-Kumar-Yedla@users.noreply.github.com>
2024-04-11 10:14:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 //complete this code
 class Rectangle {
 	constructor(width,height){
-		this._width= width;
-		this._height=height;
+		this.width= width;
+		this.height=height;
 	}
 	get width(){
-		return this._width;
+		return this.width;
 	}
 	get height(){
-		return this._height;
+		return this.height;
 	}
 	getArea(){
-	return	this._width * this._height;
+	return	`${this.width} * ${this.height}`;
 	}
 }
 
 class Square extends Rectangle {
-	// constructor(side){
-	// 	super(side,side)
-	// }
 	getPerimeter(){
-return 4 * this._width;
+return `4 * ${this.width}`;
 	}
 }
 
